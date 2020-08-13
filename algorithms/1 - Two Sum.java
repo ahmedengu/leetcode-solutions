ec8682456d6a25d89d8b320a1a15f7cbeb00873d
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-08-13 23:40:53 +0200

    Create 1 - Two Sum.java

---
+++ algorithms/1 - Two Sum.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+               int [] ret=new int[2];
+        
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if(nums[i]+nums[j]==target) {
+                    ret[0]=i+1;
+                ret[1]=j+1;
+                        return ret;
+                }
+            }
+        }
+        
+        return ret;
+    }
+}
