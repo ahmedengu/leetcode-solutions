3cd13307912c3383481030dc5cfb1bbc003c81e5
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-08-16 22:18:53 +0200

    Update 6 - ZigZag Conversion.js

--- algorithms/6 - ZigZag Conversion.js
+++ algorithms/6 - ZigZag Conversion.js
@@ -9,14 +9,15 @@ var convert = function(s, numRows) {
 
     for (const c of s) {
         ret[row] = (ret[row] || '') + c;
+        
         if (down) {
-            row++
-            if (row === numRows - 1) down = false
+            row++;
+            if (row === numRows - 1) down = false;
         } else {
             row--;
             if (row === 0) down = true;
         }
     }
 
-    return ret.join('')
+    return ret.join('');
 };
