d6157be4a530e89c51738ec07f645d43b1a0584c
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-08-23 19:35:29 +0200

    Create 206 - Reverse Linked List.js

---
+++ algorithms/206 - Reverse Linked List.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let tail = null;
+
+    while (head) {
+        tail = new ListNode(head.val, tail)
+        head = head.next;
+    }
+
+    return tail;
+};
