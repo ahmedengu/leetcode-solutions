9dfdf629cedf8940507c41c9ed31fa98d6de8aa6
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-08-28 21:36:05 +0200

    Create 47 - Permutations II.js

---
+++ algorithms/47 - Permutations II.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums, prefix = [], mem = {}, ret = []) {
+    if (!nums.length && !mem[prefix]) {
+        ret.push(prefix);
+        mem[prefix] = 1;
+    }
+
+    nums.forEach((n, i) => permuteUnique(nums.filter((v, j) => i !== j), prefix.concat(n), mem, ret));
+    
+    return ret;
+};
