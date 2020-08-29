0f485c57868a1177dea97fbdfd0cb05bc6bd6812
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-08-29 21:56:48 +0200

    Create 112 - Path Sum.js

---
+++ algorithms/112 - Path Sum.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if (!root) return false;
+
+    const curr = sum - root.val;
+    if (!root.right && !root.left) return curr === 0;
+    return hasPathSum(root.right, curr) || hasPathSum(root.left, curr);
+};
