4d477763f18d2e238ce292ed93a768c12650533f
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-09-06 21:58:17 +0200

    Create 217 - Contains Duplicate.js

---
+++ algorithms/217 - Contains Duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const map = {};
+
+    for (const num of nums) {
+        if (map[num]) return true;
+        map[num] = 1;
+    }
+
+    return false;
+};
