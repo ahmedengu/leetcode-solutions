d6b904cc6c120819ac2480ed5046df3dbb186fa7
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-10-27 21:44:45 +0200

    Create 1137 - N-th Tribonacci Number.js

---
+++ algorithms/1137 - N-th Tribonacci Number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function(n) {
+    if (mem[n] !== undefined) return mem[n];
+    for (let i = 3; i <= n; i++) {
+        mem[i] = mem[i - 1] + mem[i - 2] + mem[i - 3];
+    }
+    return mem[n];
+};
+
+const mem = {
+    0: 0,
+    1: 1,
+    2: 1
+};
