f21a1c95b0558c9e644f55aad556eeca9d55372e
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-10-28 13:14:46 +0200

    Create 1160 - Find Words That Can Be Formed by Characters.js

---
+++ algorithms/1160 - Find Words That Can Be Formed by Characters.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string[]} words
+ * @param {string} chars
+ * @return {number}
+ */
+var countCharacters = function(words, chars) {
+    const map = _.countBy(chars);
+    return words.reduce((acc, word) => acc += _.every(_.countBy(word), (v, k) => map[k] >= v) ? word.length : 0, 0);
+};
