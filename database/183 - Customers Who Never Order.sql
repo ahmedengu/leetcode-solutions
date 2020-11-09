4563ea23905d342312e5649453c190c041c89e05
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-11-09 22:30:13 +0200

    Create 183 - Customers Who Never Order.sql

---
+++ database/183 - Customers Who Never Order.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT
+   Name AS Customers 
+FROM
+   Customers 
+   LEFT JOIN
+      Orders 
+      ON Customers.Id = Orders.CustomerId 
+WHERE
+   Orders.Id IS NULL
