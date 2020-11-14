108ba6e8cfb5c775be2e95e779dd00470c828537
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-11-14 22:34:24 +0200

    Create 94 - Binary Tree Inorder Traversal.js

---
+++ algorithms/94 - Binary Tree Inorder Traversal.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const ret = [];
+    const s = [];
+    let node = root;
+
+    while (node || s.length) {
+        if (node) {
+            s.push(node);
+            node = node.left;
+        } else {
+            node = s.pop();
+            ret.push(node.val);
+            node = node.right;
+        }
+    }
+
+    return ret;
+};
