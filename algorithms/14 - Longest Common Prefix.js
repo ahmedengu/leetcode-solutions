ac447b1a68e0c75d18746ce21cec922c5f2e14ad
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-11-18 23:30:24 +0200

    Create 14 - Longest Common Prefix.js

---
+++ algorithms/14 - Longest Common Prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (!strs || !strs.length) return '';
+
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            if (strs[j][i] !== strs[0][i]) return strs[0].slice(0, i);
+        }
+    }
+
+    return strs[0] || '';
+};
