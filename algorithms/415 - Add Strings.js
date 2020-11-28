e856c91807b4b5ee16a7fb7cb6c2a87bd2af7539
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-11-28 22:15:37 +0200

    Update 415 - Add Strings.js

--- algorithms/415 - Add Strings.js
+++ algorithms/415 - Add Strings.js
@@ -5,27 +5,13 @@
  */
 var addStrings = function(num1, num2) {
     let ret = '';
-    let i = num1.length - 1;
-    let j = num2.length - 1;
+    let i = num1.length;
+    let j = num2.length;
     let carry = 0;
     let tmp;
 
-    while (i >= 0 && j >= 0) {
-        tmp = (+num1[i--]) + (+num2[j--]) + carry;
-        carry = tmp > 9 ? 1 : 0;
-        tmp %= 10;
-        ret = tmp + ret;
-    }
-
-    while (i >= 0) {
-        tmp = (+num1[i--]) + carry;
-        carry = tmp > 9 ? 1 : 0;
-        tmp %= 10;
-        ret = tmp + ret;
-    }
-
-    while (j >= 0) {
-        tmp = (+num2[j--]) + carry;
+    while (i > 0 || j > 0) {
+        tmp = (i && +num1[--i]) + (j && +num2[--j]) + carry;
         carry = tmp > 9 ? 1 : 0;
         tmp %= 10;
         ret = tmp + ret;
