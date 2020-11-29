e86aa6830a7caf8f7a219f6e613722aac2095da4
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-11-29 23:22:57 +0200

    Create 1266 - Minimum Time Visiting All Points.js

---
+++ algorithms/1266 - Minimum Time Visiting All Points.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minTimeToVisitAllPoints = function(points) {
+    let ret = 0,
+        i = 1,
+        len = points.length;
+
+    for (; i < len; i += 1) {
+        ret += Math.max(Math.abs(points[i - 1][0] - points[i][0]),
+            Math.abs(points[i - 1][1] - points[i][1]));
+    }
+
+    return ret;
+};
