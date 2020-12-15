abaddc33b62f81678707b1f8079bc101a8c507c8
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-12-15 23:16:26 +0200

    Update 318 - Maximum Product of Word Lengths.js

--- algorithms/318 - Maximum Product of Word Lengths.js
+++ algorithms/318 - Maximum Product of Word Lengths.js
@@ -6,18 +6,18 @@ var maxProduct = function(words) {
     let ret = 0;
 
     for (let i = 0; i < words.length; i++) {
+        const set = new Set([...words[i]]);
         for (let j = i + 1; j < words.length; j++) {
-            if (checkInteresction(words[i], words[j])) ret = Math.max(ret, words[i].length * words[j].length);
+            if (checkInteresction(words[j], set)) ret = Math.max(ret, words[i].length * words[j].length);
         }
     }
 
     return ret;
 };
 
-const checkInteresction = (word1, word2) => {
-    for (let i = 0; i < word1.length; i++) {
-        if (word2.includes(word1[i])) return false;
+const checkInteresction = (word, set) => {
+    for (let i = 0; i < word.length; i++) {
+        if (set.has(word[i])) return false;
     }
-
     return true;
 }
