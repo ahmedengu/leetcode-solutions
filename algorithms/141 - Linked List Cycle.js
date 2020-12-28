604cd0cefa4fab4d968006a4f2aad27b4c3c9721
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-12-28 22:55:59 +0200

    Create 141 - Linked List Cycle.js

---
+++ algorithms/141 - Linked List Cycle.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if (!head) return false;
+    if (head.f) return true;
+    head.f = true;
+    return hasCycle(head.next);
+};
