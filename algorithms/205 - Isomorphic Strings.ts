fee2e7a91181bc5b13de17299fbf32871ef0b513
Ahmed Kamel Taha <ahmedengu@gmail.com>
2021-01-03 23:31:29 +0200

    Update 205 - Isomorphic Strings.ts

--- algorithms/205 - Isomorphic Strings.ts
+++ algorithms/205 - Isomorphic Strings.ts
@@ -1,14 +1,14 @@
 function isIsomorphic(s: string, t: string): boolean {
     const map: { [key: string]: string } = {};
-    const inv: { [key: string]: string } = {};
+    const set = new Set();
     
     for (let i = 0; i < s.length; i++) {
         if (map[s[i]] !== t[i]) {
-            if (map[s[i]] || inv[t[i]]) {
+            if (map[s[i]] || set.has(t[i])) {
                 return false;
             } else {
                 map[s[i]] = t[i];
-                inv[t[i]] = s[i];
+                set.add(t[i]);
             }
         }
     }
