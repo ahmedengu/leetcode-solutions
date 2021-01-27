63c4162e4ba3408527d8fd8869b1850aec10fbe2
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-01-27 23:32:50 +0200

    Update 219 - Contains Duplicate II.ts

--- algorithms/219 - Contains Duplicate II.ts
+++ algorithms/219 - Contains Duplicate II.ts
@@ -1,6 +1,9 @@
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
-    for (let i = 0; i < nums.length; i++)
-        for (let j = Math.max(0, i - k); j < i; j++)
-            if (nums[i] === nums[j]) return true;
+    const set = new Set();
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) return true;
+        set.add(nums[i]);
+        if (set.size > k) set.delete(nums[i - k]);
+    }
     return false;
 };
