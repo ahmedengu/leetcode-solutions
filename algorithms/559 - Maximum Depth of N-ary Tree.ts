3e03276fa0ddeb24f93ebcec088125f60cd7be8b
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-03-17 23:12:51 +0200

    Create 559 - Maximum Depth of N-ary Tree.ts

---
+++ algorithms/559 - Maximum Depth of N-ary Tree.ts
@@ -0,0 +1,15 @@
+/**
+ * Definition for Node.
+ * class Node {
+ *     val: number
+ *     children: Node[]
+ *     constructor(val?: number, children?: Node[]) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.children = (children===undefined ? [] : children)
+ *     }
+ * }
+ */
+
+function maxDepth(root: Node): number {
+    return root ? Math.max(0, ..._.map(root.children, maxDepth)) + 1 : 0;
+};
