67f4d4af9617f2e382c60a2ebcdad8bbd2ef974d
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-04-02 23:23:49 +0300

    Create 628 - Maximum Product of Three Numbers.ts

---
+++ algorithms/628 - Maximum Product of Three Numbers.ts
@@ -0,0 +1,4 @@
+function maximumProduct(nums: number[]): number {
+    nums.sort((a, b) => b - a);
+    return Math.max(nums[0] * nums[1] * nums[2], nums[0] * nums[nums.length - 2] * nums[nums.length - 1]);
+};
