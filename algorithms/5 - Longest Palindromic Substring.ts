4440e8e30f9750b9d2f9431ed9319bb139c707da
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-04-06 00:10:54 +0300

    Update 5 - Longest Palindromic Substring.ts

--- algorithms/5 - Longest Palindromic Substring.ts
+++ algorithms/5 - Longest Palindromic Substring.ts
@@ -1,20 +1,14 @@
 function longestPalindrome(s: string): string {
-    const len = s.length;
-    let ret = '';
-    const dp = _.times(len, (i) => {
-        const arr = _.times(len, () => false);
-        arr[i] = true;
-        ret = s[i];
-        return arr;
-    });
+    let max = '';
+    for (let i = 0; i < s.length; i++)
+        for (let j = s.length - 1; j >= i; j--)
+            if (max.length < j - i + 1 && isPalindrom(s, i, j)) max = s.slice(i, j + 1);
+    return max;
+};
 
-    for (let i = 0; i < len; i++)
-        (s[i] === s[i + 1]) && (dp[i][i + 1] = true, ret = s[i] + s[i + 1]);
+const isPalindrom = (s, l, r) => {
+    while (l < r)
+        if (s[l++] !== s[r--]) return false;
 
-
-    for (let i = len - 1; i >= 0; i--)
-        for (let j = i + 2; j < len; j++)
-            ((dp[i][j] = dp[i + 1][j - 1] && s[i] === s[j]) && ret.length < j + 1 - i) && (ret = s.slice(i, j + 1));
-
-    return ret;
+    return true;
 };
