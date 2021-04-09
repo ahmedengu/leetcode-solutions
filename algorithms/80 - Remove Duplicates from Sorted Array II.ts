e3c0b5f8ef180111fd28cac7f572970c17b55186
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-04-09 22:59:16 +0300

    Create 80 - Remove Duplicates from Sorted Array II.ts

---
+++ algorithms/80 - Remove Duplicates from Sorted Array II.ts
@@ -0,0 +1,5 @@
+function removeDuplicates(nums: number[]): number {
+    for (let i = 2; i < nums.length; i++)
+        if (nums[i] === nums[i - 2]) nums.splice(--i, 1);
+    return nums.length;
+};
