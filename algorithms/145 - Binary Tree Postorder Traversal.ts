89b44eb4da82b780491323a6eb2246b36b4ba063
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-04-12 22:16:49 +0300

    Create 145 - Binary Tree Postorder Traversal.ts

---
+++ algorithms/145 - Binary Tree Postorder Traversal.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null, ret = []): number[] {
+    if (!root) return ret;
+    
+    postorderTraversal(root.left, ret);
+    postorderTraversal(root.right, ret);
+    ret.push(root.val);
+    
+    return ret;
+};
