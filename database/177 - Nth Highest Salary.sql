a24dc3c322dbce9cbff57018bb726323eefee76a
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-04-16 23:34:40 +0300

    Create 177 - Nth Highest Salary.sql

---
+++ database/177 - Nth Highest Salary.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT 
+BEGIN
+SET N = N - 1;
+RETURN (
+    SELECT DISTINCT
+       Salary 
+    FROM
+       Employee 
+    ORDER BY
+       Salary DESC
+    LIMIT 1
+    OFFSET N
+);
+END
