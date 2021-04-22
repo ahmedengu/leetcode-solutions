fef32540eec17bd3d6b9bfc6f2e9d3e71dd31c5b
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-04-22 16:07:21 +0300

    Update 238 - Product of Array Except Self.ts

--- algorithms/238 - Product of Array Except Self.ts
+++ algorithms/238 - Product of Array Except Self.ts
@@ -1,9 +1,5 @@
 function productExceptSelf(nums: number[]): number[] {
     let zeros = 0;
     const product = nums.reduce((acc, num) => num ? acc * num : ++zeros && acc, 1);
-    return nums.map(num => {
-        if (num && !zeros) return product / num;
-        if (!num && zeros === 1) return product;
-        return 0;
-    });
+    return nums.map(num => (num && !zeros || !num && zeros === 1 ? product : 0) / (num && !zeros ? num : 1));
 };
