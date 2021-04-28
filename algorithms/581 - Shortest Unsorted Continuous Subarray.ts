a4b4b2eb0c54f5e7159652c24347fd46887891d1
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-04-28 23:20:08 +0300

    Update 581 - Shortest Unsorted Continuous Subarray.ts

--- algorithms/581 - Shortest Unsorted Continuous Subarray.ts
+++ algorithms/581 - Shortest Unsorted Continuous Subarray.ts
@@ -1,6 +1,5 @@
 function findUnsortedSubarray(nums: number[]): number {
-    const sorted = _.sortBy(nums);
-    for (var l = 0; l < nums.length && nums[l] === sorted[l]; l++);
-    for (var r = nums.length - 1; r >= l && nums[r] === sorted[r]; r--);
-    return r - l + 1;
+    return _.sortBy(nums)
+        .reduce((acc, num, i) => acc + (num === nums[i] ? ' ' : '*'), '')
+        .trim().length;
 };
