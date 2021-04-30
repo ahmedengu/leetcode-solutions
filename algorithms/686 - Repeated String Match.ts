95f78373129673150c3675975f85902624eb3d7c
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-04-30 18:55:41 +0300

    Update 686 - Repeated String Match.ts

--- algorithms/686 - Repeated String Match.ts
+++ algorithms/686 - Repeated String Match.ts
@@ -1,6 +1,6 @@
 function repeatedStringMatch(a: string, b: string): number {
-    const q = Math.floor((b.length - 1) / a.length + 1);
-    for (let i = 0; i < 2; i++)
-        if (a.repeat(q + i).includes(b)) return q + i;
+    let q = Math.floor((b.length - 1) / a.length);
+    if (a.repeat(++q).includes(b)) return q;
+    if (a.repeat(++q).includes(b)) return q;
     return -1;
 };
