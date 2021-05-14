3ff8373beb9d8b5555794ead50f4d83a2d1fdee5
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-05-14 04:02:59 +0300

    Update 1848 - Minimum Distance to the Target Element.ts

--- algorithms/1848 - Minimum Distance to the Target Element.ts
+++ algorithms/1848 - Minimum Distance to the Target Element.ts
@@ -1,3 +1,3 @@
 function getMinDistance(nums: number[], target: number, start: number): number {
-    return nums.reduce((acc, v, i) => v === target ? Math.min(acc, Math.abs(start - i)) : acc, Infinity);
+    return nums.reduce((min, num, i) => num === target ? Math.min(min, Math.abs(start - i)) : min, Infinity);
 };
