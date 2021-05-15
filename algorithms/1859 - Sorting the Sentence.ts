db367e469310fb2b01842d333f24ca99f3b47396
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-05-15 23:00:34 +0300

    Update 1859 - Sorting the Sentence.ts

--- algorithms/1859 - Sorting the Sentence.ts
+++ algorithms/1859 - Sorting the Sentence.ts
@@ -1,5 +1,8 @@
 function sortSentence(s: string): string {
-    return _.sortBy(s.split(' '), str => str.slice(-1))
+    return _
+        .chain(s)
+        .split(' ')
+        .sortBy(str => +str.slice(-1))
         .map(str => str.slice(0, -1))
         .join(' ');
 };
