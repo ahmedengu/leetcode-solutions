267b607dc3a9be979e0ef0a3d1c759c945cc13a7
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-05-18 15:09:44 +0300

    Update 1827 - Minimum Operations to Make the Array Increasing.ts

--- algorithms/1827 - Minimum Operations to Make the Array Increasing.ts
+++ algorithms/1827 - Minimum Operations to Make the Array Increasing.ts
@@ -1,12 +1,9 @@
 function minOperations(nums: number[]): number {
-    let ret = 0;
-    
-    for (let i = 1; i < nums.length; i++) {
+    return nums.reduce((acc, v, i) => {
         if (nums[i] <= nums[i - 1]) {
-            ret += nums[i - 1] - nums[i] + 1;
+            acc += nums[i - 1] - nums[i] + 1;
             nums[i] += nums[i - 1] - nums[i] + 1;
         }
-    }
-    
-    return ret;
+        return acc;
+    }, 0);
 };
