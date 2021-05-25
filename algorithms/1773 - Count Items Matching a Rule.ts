c8bfbd86bb2816d672fb830d62ff757b776ec5e6
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-05-25 17:32:30 +0300

    Update 1773 - Count Items Matching a Rule.ts

--- algorithms/1773 - Count Items Matching a Rule.ts
+++ algorithms/1773 - Count Items Matching a Rule.ts
@@ -1,8 +1,8 @@
 function countMatches(items: string[][], ruleKey: string, ruleValue: string): number {
-    const map = {
+    const key = {
         'type': 0,
         'color': 1,
         'name': 2
-    };
-    return items.filter(item => item[map[ruleKey]] === ruleValue).length;
+    } [ruleKey];
+    return items.filter(item => item[key] === ruleValue).length;
 };
