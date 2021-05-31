25bb4fb91a535661fc12c4952de250a849af4398
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-06-01 00:49:56 +0300

    Update 1700 - Number of Students Unable to Eat Lunch.ts

--- algorithms/1700 - Number of Students Unable to Eat Lunch.ts
+++ algorithms/1700 - Number of Students Unable to Eat Lunch.ts
@@ -1,7 +1,6 @@
 function countStudents(students: number[], sandwiches: number[]): number {
     const count = _.countBy(students);
     for (const s of sandwiches)
-        if (count[s]) count[s]--;
-        else return count[1 - s];
+        if (!count[s]--) return count[1 - s];
     return 0;
 };
