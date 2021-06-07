162dda96da03e152ecf8349feafd76d006e8a5bf
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-06-07 21:50:21 +0300

    Create 470 - Implement Rand10() Using Rand7().ts

---
+++ algorithms/470 - Implement Rand10() Using Rand7().ts
@@ -0,0 +1,15 @@
+/**
+ * The rand7() API is already defined for you.
+ * function rand7(): number {}
+ * @return a random integer in the range 1 to 7
+ */
+
+function rand10(): number {
+    let row, col, idx;
+    do {
+        row = rand7();
+        col = rand7();
+        idx = col + (row - 1) * 7;
+    } while (idx > 40);
+    return 1 + (idx - 1) % 10;
+};
