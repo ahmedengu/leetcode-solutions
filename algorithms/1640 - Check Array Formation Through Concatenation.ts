f2e9acaa105e340047ad7aefbb14ea29e48ed037
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-07-04 23:10:34 +0300

    Update 1640 - Check Array Formation Through Concatenation.ts

--- algorithms/1640 - Check Array Formation Through Concatenation.ts
+++ algorithms/1640 - Check Array Formation Through Concatenation.ts
@@ -1,7 +1,8 @@
 function canFormArray(arr: number[], pieces: number[][]): boolean {
+    pieces.sort((a, b) => b.length - a.length);
     let str = `,${arr},`;
-    for (const piece of pieces) {
-        str = str.replace(`,${piece},`, ',');
+    for (let i = 0; i < pieces.length; i++) {
+        str = str.replace(`,${pieces[i]},`, ',');
     }
     str = str.replace(/,/g, '');
     return !str.length;
