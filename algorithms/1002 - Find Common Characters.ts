b82b1354e371f2d0886c1de2aa3f4829ed405e67
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-07-24 19:29:19 +0300

    Create 1002 - Find Common Characters.ts

---
+++ algorithms/1002 - Find Common Characters.ts
@@ -0,0 +1,4 @@
+function commonChars(words: string[]): string[] {
+    const counts = words.map(x => _.countBy(x)).reduce((acc, x) => _.mapValues(acc, (v, k) => x[k] && (x[k] < v ? x[k] : v)));
+    return _.reduce(counts, (acc, v, k) => acc.concat(_.times(v, _.constant(k))), []);
+};
