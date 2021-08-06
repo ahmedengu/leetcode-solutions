10055561dc899db99ee44b5e93e991abf120168a
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-08-06 23:46:31 +0300

    Update 1315 - Sum of Nodes with Even-Valued Grandparent.ts

--- algorithms/1315 - Sum of Nodes with Even-Valued Grandparent.ts
+++ algorithms/1315 - Sum of Nodes with Even-Valued Grandparent.ts
@@ -15,14 +15,13 @@
 function sumEvenGrandparent(root: TreeNode | null): number {
     let sum = 0;
 
-    const traverse = (node: TreeNode | null, p = 1, gp = 1) => {
+    (function traverse(node: TreeNode | null, p = 1, gp = 1) {
         if (node) {
             if (gp % 2 === 0) sum += node.val;
             traverse(node.left, node.val, p);
             traverse(node.right, node.val, p);
         }
-    }
-    traverse(root);
+    })(root);
 
     return sum;
 };
