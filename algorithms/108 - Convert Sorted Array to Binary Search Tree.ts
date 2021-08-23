1929a65ce490cde969f767f92d9a6ce7e23508d0
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-08-23 22:49:32 +0300

    Create 108 - Convert Sorted Array to Binary Search Tree.ts

---
+++ algorithms/108 - Convert Sorted Array to Binary Search Tree.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+    if (!nums || !nums.length) return null;
+
+    const mid = Math.floor(nums.length / 2);
+    const ret = new TreeNode(nums[mid]);
+
+    ret.left = sortedArrayToBST(nums.slice(0, mid));
+    ret.right = sortedArrayToBST(nums.slice(mid + 1));
+
+    return ret;
+};
