490ed86755c5502d492dcfc6eee76f5872f6d6e4
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-09-10 22:32:21 +0300

    Create 112 - Path Sum.ts

---
+++ algorithms/112 - Path Sum.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function hasPathSum(root: TreeNode | null, sum: number): boolean {
+    if (!root) return false;
+
+    const curr = sum - root.val;
+    if (!root.right && !root.left) return curr === 0;
+    return hasPathSum(root.right, curr) || hasPathSum(root.left, curr);
+
+};
