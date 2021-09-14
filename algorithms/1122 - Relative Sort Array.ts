51160e8037c1996503aa7bd30ee971fd4636da3c
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-09-14 22:38:54 +0300

    Create 1122 - Relative Sort Array.ts

---
+++ algorithms/1122 - Relative Sort Array.ts
@@ -0,0 +1,15 @@
+function relativeSortArray(arr1: number[], arr2: number[]): number[] {
+    const ret = [];
+
+    for (const n of arr2) {
+        let tmp = arr1.filter(x => x === n);
+        if (tmp.length) {
+            arr1 = arr1.filter(x => x !== n);
+            ret.push(...tmp);
+        }
+    }
+
+    arr1.sort((a, b) => a - b);
+
+    return ret.concat(arr1);
+};
