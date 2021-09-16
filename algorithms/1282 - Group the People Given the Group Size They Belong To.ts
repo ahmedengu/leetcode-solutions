eee1c576f360b5458ff776b8975f763eac450e88
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-09-16 23:24:57 +0300

    Create 1282 - Group the People Given the Group Size They Belong To.ts

---
+++ algorithms/1282 - Group the People Given the Group Size They Belong To.ts
@@ -0,0 +1,17 @@
+function groupThePeople(groupSizes: number[]): number[][] {
+    const ret = [];
+    const groups = {};
+    for (let i = 0; i < groupSizes.length; i++)
+        (groups[groupSizes[i]] || (groups[groupSizes[i]] = [])).push(i);
+    for (const [key, value] of Object.entries < number[] > (groups)) {
+        ret.push([]);
+        while (value.length) {
+            if (ret[ret.length - 1].length >= Number(key)) {
+                ret.push([]);
+            }
+            ret[ret.length - 1].push(value.pop());
+        }
+    }
+
+    return ret;
+};
