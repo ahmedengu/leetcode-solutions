ad7c0a7913a66ca6ee48471e291663fa9d8004f7
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-10-09 23:26:39 +0300

    Update 1002 - Find Common Characters.js

--- algorithms/1002 - Find Common Characters.js
+++ algorithms/1002 - Find Common Characters.js
@@ -4,7 +4,6 @@
  */
 var commonChars = function(A) {
     const counts = A.map(x => _.countBy(x))
-        .reduce((acc, x) => _.mapValues(acc, (v, k) => x[k] && (x[k] < v ? x[k] : v)));
-
+        .reduce((acc, x) => _.mapValues(acc, (v, k) => x[k] && (x[k] < v ? x[k] : v))); 
     return _.reduce(counts, (acc, v, k) => acc.concat(_.times(v, _.constant(k))), []);
 };
