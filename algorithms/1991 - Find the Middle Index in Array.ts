4386ecfc89195f4b9c6b6a8be50f2b4783c059eb
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-10-16 19:50:53 +0300

    Update 1991 - Find the Middle Index in Array.ts

--- algorithms/1991 - Find the Middle Index in Array.ts
+++ algorithms/1991 - Find the Middle Index in Array.ts
@@ -1,8 +1,6 @@
 function findMiddleIndex(nums: number[]): number {
     const sum = _.sum(nums);
-    for (let i = 0; i < nums.length; i++) {
-        const s = _.sum(nums.slice(0, i));
-        if (s === sum - s - nums[i]) return i;
-    }
+    for (let i = 0; i < nums.length; i++)
+        if (2 * _.sum(nums.slice(0, i)) === sum - nums[i]) return i;
     return -1;
 };
