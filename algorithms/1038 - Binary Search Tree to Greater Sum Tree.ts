7b5c0b80eb4721cb1eff7ab259ff5eab03111c77
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-11-10 01:44:09 +0200

    Create 1038 - Binary Search Tree to Greater Sum Tree.ts

---
+++ algorithms/1038 - Binary Search Tree to Greater Sum Tree.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function bstToGst(root: TreeNode | null, o = { sum: 0 }): TreeNode | null {
+    if (root) {
+        bstToGst(root.right, o);
+        root.val = (o.sum += root.val);
+        bstToGst(root.left, o);
+    }
+    return root;
+};
