e30a1d6b04d17314e7e21f8d78aa7d5b30203d44
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-12-19 19:08:35 +0200

    Update 1170 - Compare Strings by Frequency of the Smallest Character.ts

--- algorithms/1170 - Compare Strings by Frequency of the Smallest Character.ts
+++ algorithms/1170 - Compare Strings by Frequency of the Smallest Character.ts
@@ -3,4 +3,5 @@ function numSmallerByFrequency(queries: string[], words: string[]): number[] {
     return queries.map(minCount).map(q => wordsCount.filter(w => q < w).length);
 };
 
+
 const minCount = (s: string): number => _.countBy(s)[_.min(s)];
