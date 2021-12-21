22aa55741de6b0ddaabf1363b5f68253e9cf1d88
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-12-21 21:21:16 +0200

    Update 125 - Valid Palindrome.js

--- algorithms/125 - Valid Palindrome.js
+++ algorithms/125 - Valid Palindrome.js
@@ -2,6 +2,7 @@
  * @param {string} s
  * @return {boolean}
  */
+
 var isPalindrome = function(s) {
     s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
     return s === s.split('').reverse().join('');
