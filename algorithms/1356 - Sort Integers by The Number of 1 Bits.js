3ae91093f3a42de9bab60508732568da2bd94887
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-12-22 23:05:51 +0200

    Update 1356 - Sort Integers by The Number of 1 Bits.js

--- algorithms/1356 - Sort Integers by The Number of 1 Bits.js
+++ algorithms/1356 - Sort Integers by The Number of 1 Bits.js
@@ -6,7 +6,6 @@ var sortByBits = function(arr) {
     return arr.sort((a, b) => {
         if (!(a in map)) map[a] = a.toString(2).replace(/0/g, '').length;
         if (!(b in map)) map[b] = b.toString(2).replace(/0/g, '').length;
-
         return (map[a] - map[b]) || (a - b);
     });
 };
