5d44cbd68194773f3b6d1521e24d527e32559bd0
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-12-25 16:48:31 +0200

    Update 1160 - Find Words That Can Be Formed by Characters.js

--- algorithms/1160 - Find Words That Can Be Formed by Characters.js
+++ algorithms/1160 - Find Words That Can Be Formed by Characters.js
@@ -5,5 +5,6 @@
  */
 var countCharacters = function(words, chars) {
     const map = _.countBy(chars);
+
     return words.reduce((acc, word) => acc += _.every(_.countBy(word), (v, k) => map[k] >= v) ? word.length : 0, 0);
 };
