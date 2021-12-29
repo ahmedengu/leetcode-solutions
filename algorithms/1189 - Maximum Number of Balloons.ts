316ca38a3e6e83152317aa944a3923ac36aa7c9b
Ahmed Aboumalwa <ahmedengu@gmail.com>
2021-12-29 23:59:26 +0200

    Update 1189 - Maximum Number of Balloons.ts

--- algorithms/1189 - Maximum Number of Balloons.ts
+++ algorithms/1189 - Maximum Number of Balloons.ts
@@ -1,6 +1,5 @@
 function maxNumberOfBalloons(text: string): number {
     const count = _.countBy(text);
     const min = Math.min(count['b'], count['a'], Math.floor(count['l'] / 2), Math.floor(count['o'] / 2), count['n']);
-
     return isFinite(min) && min;
 };
