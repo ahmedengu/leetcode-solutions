15eafd9578da020ac1fbefeec7d3c136d4baab8f
Ahmed Aboumalwa <ahmedengu@gmail.com>
2022-01-02 18:58:57 +0200

    Update 2073 - Time Needed to Buy Tickets.ts

--- algorithms/2073 - Time Needed to Buy Tickets.ts
+++ algorithms/2073 - Time Needed to Buy Tickets.ts
@@ -1,5 +1,6 @@
 function timeRequiredToBuy(tickets: number[], k: number): number {
     let ret = 0;
+
     while (tickets[k]) {
         for (let i = 0; i < tickets.length && tickets[k]; i++) {
             if (tickets[i]) {
