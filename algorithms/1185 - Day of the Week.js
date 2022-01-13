384e33b4290201c5e0f9247fb391d06432a2a6d8
Ahmed Aboumalwa <ahmedengu@gmail.com>
2022-01-13 23:14:27 +0200

    Update 1185 - Day of the Week.js

--- algorithms/1185 - Day of the Week.js
+++ algorithms/1185 - Day of the Week.js
@@ -4,6 +4,7 @@
  * @param {number} year
  * @return {string}
  */
+
 var dayOfTheWeek = function(day, month, year) {
     return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][new Date(year, month - 1, day).getDay()];
 };
