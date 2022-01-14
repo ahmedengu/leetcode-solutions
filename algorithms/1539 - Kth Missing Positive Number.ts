4399646a8eb6969f00ab3dee4382f0f445753f30
Ahmed Aboumalwa <ahmedengu@gmail.com>
2022-01-14 21:40:49 +0200

    Update 1539 - Kth Missing Positive Number.ts

--- algorithms/1539 - Kth Missing Positive Number.ts
+++ algorithms/1539 - Kth Missing Positive Number.ts
@@ -1,5 +1,6 @@
 function findKthPositive(arr: number[], k: number): number {
     const set = new Set(arr);
+
     for (let i = 1; true; i++) {
         if (!set.has(i) && !(--k)) {
             return i;
