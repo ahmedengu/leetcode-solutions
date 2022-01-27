f51ddb51631d9d80bf31755e5f620da30e4f03c7
Ahmed Aboumalwa <ahmedengu@gmail.com>
2022-01-27 21:58:33 +0200

    Update 1 - Two Sum.ts

--- algorithms/1 - Two Sum.ts
+++ algorithms/1 - Two Sum.ts
@@ -1,11 +1,10 @@
 function twoSum(nums: number[], target: number): number[] {
-    const map = nums.reduce((acc, v, i) => {
-        acc[v] = i;
-        return acc;
+    const map = nums.reduce((obj, num, i) => {
+        obj[num] = i;
+        return obj;
     }, {});
-    for (let i = 0; i < nums.length; i++) {
-        const idx = map[target - nums[i]];
-        if (idx >= 0 && idx !== i)
-            return [i, idx];
-    }
+
+    for (let i = 0; i < nums.length; i++)
+        if (map[target - nums[i]] > i)
+            return [i, map[target - nums[i]]];
 };
