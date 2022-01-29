07468babc9e699bafc2a3bffebb9a0f008cd1e5e
Ahmed Aboumalwa <ahmedengu@gmail.com>
2022-01-29 21:21:31 +0200

    Update 1071 - Greatest Common Divisor of Strings.js

--- algorithms/1071 - Greatest Common Divisor of Strings.js
+++ algorithms/1071 - Greatest Common Divisor of Strings.js
@@ -5,7 +5,6 @@
  */
 var gcdOfStrings = function(str1, str2) {
     if (str1 + str2 !== str2 + str1) return '';
-
     const gcd = (a, b) => a === 0 ? b : gcd( b % a , a);
     return str1.substring(0, gcd(str1.length, str2.length));
 };
