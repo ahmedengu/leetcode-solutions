c7715925d553aa757bdcf74f0144dad1cf3362cf
Ahmed Aboumalwa <ahmedengu@gmail.com>
2022-02-03 23:21:03 +0200

    Create 94 - Binary Tree Inorder Traversal.ts

---
+++ algorithms/94 - Binary Tree Inorder Traversal.ts
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null, arr = []): number[] {
+    if (!root) return arr;
+    inorderTraversal(root.left, arr);
+    arr.push(root.val)
+    inorderTraversal(root.right, arr);
+    return arr;
+};
