0e76faad0b043622ad9a47d54e253640460927e6
Ahmed Aboumalwa <ahmedengu@gmail.com>
2023-06-25 19:09:02 +0300

    Create 2695 - Array Wrapper.ts

---
+++ algorithms/2695 - Array Wrapper.ts
@@ -0,0 +1,22 @@
+class ArrayWrapper {
+    nums: number[];
+	constructor(nums: number[]) {
+        this.nums = nums;
+    }
+
+	valueOf() {
+        return _.sum(this.nums);
+    }
+
+	toString() {
+       return `[${this.nums.join()}]` 
+    }
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
